38006d288d377ce4e9a8ba3798a66e1e54293177
Ruhan Khandakar <khandakarruhan@gmail.com>
2022-06-19 14:23:33 +0530

    fix: (week10) responsiveness

--- Week-10/shortly/src/components/Shortner/Shortner.jsx
+++ Week-10/shortly/src/components/Shortner/Shortner.jsx
@@ -6,10 +6,10 @@ const Shortner = () => {
   return (
     <div className="flex flex-col items-center justify-between xs:flex-row">
       <div className="details">
-        <h1 className="text-2xl mt-4  text-center md:text-5xl md:mt-0  xs:text-left">
+        <h1 className="text-2xl mt-4  text-center xs:text-5xl md:mt-0  xs:text-left">
           More than just shorter links
         </h1>
-        <p className="text-sm md:text-xl mt-2 text-center md:mt-8 xs:text-left ">
+        <p className="text-sm mt-2 text-center xs:mt-8 xs:text-left xs:text-xl">
           Build your brand's recognition and <br /> get detailed insights on how
           your links are performing
         </p>
@@ -18,7 +18,7 @@ const Shortner = () => {
         <img
           src={ComputerVector}
           alt="computer_vector"
-          className="h-[200px] w-full"
+          className="h-[300px] w-full"
         />
       </div>
     </div>
